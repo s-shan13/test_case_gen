dc02b94d07f06fe3101164c108dbfe37f9638d14
shan <shantanusuwarnkar@gmail.comD>
2022-07-05 17:38:13 +0100

    added header

---
+++ code/src/GenerateStrCase.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GenerateStrCase() {
+  return (
+    <div>GenerateStrCase</div>
+  )
+}
---
+++ code/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+import GenerateIntCase from './GenerateIntCase'
+
+export default function Header() {
+
+    
+
+  return (
+    <div className='mainDiv' >
+        <p >Integer</p>
+        <div>|</div>
+        <p>String</p>
+    </div>
+  )
+}
