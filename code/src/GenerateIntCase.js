77afe0bc902a6a201d1c525b026fc1222fa57e61
shan <shantanusuwarnkar@gmail.comD>
2022-07-15 09:44:53 +0100

    Refactored for reusability

--- code/src/GenerateIntCase.js
+++ code/src/GenerateIntCase.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import './IntGen.css'
+import './genLayout.css'
 
 export default function GenerateIntCase() {
   return (
-    <div className='intCaseBody'>
+    <div className='caseBody'>
       
       <div className='inputSection'>
         <div className='eachInput'>
-          <label for='minSize'>Enter minimum number of values to generate</label>
+          <label for='minSize'>Enter minimum number of integers to generate</label>
           <input type='text' placeholder='Min number of values' name='minSize' />
         </div>
         <div className='eachInput'>
-          <label for='maxSize'>Enter maximum number of value to generate</label>
+          <label for='maxSize'>Enter maximum number of integers to generate</label>
           <input type='text' placeholder='Max number of values' name='maxSize' />
         </div>
         <div className='eachInput'>
-          <label for='minVal'>Enter minimum value of element</label>
+          <label for='minVal'>Enter minimum value of an integer</label>
           <input type='text' placeholder='Min integer value' name='minVal' />
         </div>
         <div className='eachInput'>
-          <label for='maxVal'>Enter maximum value of element</label>
+          <label for='maxVal'>Enter maximum value an integer</label>
           <input type='text' placeholder='Max integer value' name='maxVal' />
         </div>
         <div className='eachInput'>
