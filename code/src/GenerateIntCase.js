691782290b4a85a449273c07647b714cd49ef0a7
shan <shantanusuwarnkar@gmail.comD>
2022-07-15 16:31:09 +0100

    created int gen

--- code/src/GenerateIntCase.js
+++ code/src/GenerateIntCase.js
@@ -1,41 +1,73 @@
 import React from 'react'
 import './genLayout.css'
 
+// takes long time after input size > 10^5
+
 export default function GenerateIntCase() {
+
+  const [constraints, setConstraints] = React.useState(
+    {
+      size: 10, minVal: 0, maxVal: 10
+    }
+  )
+ 
+  const [res, setRes] = React.useState([])
+
+  function handleInput(event){ 
+    setConstraints(prevCons => {
+      return {...prevCons, [event.target.name]: event.target.value}
+    })
+  }
+  
+  function generate(){
+    let tempArr = ['invalid constraints']
+    if(constraints.maxSize < constraints.minSize || constraints.maxVal < constraints.minVal){
+      console.log(3)
+    }else{
+      
+      tempArr = []
+      
+      let arrSize = parseInt(constraints.size)
+      let randomRange = parseInt(constraints.maxVal) - parseInt(constraints.minVal)
+
+      for(let i = 0; i< arrSize; i++){
+        tempArr.push(parseInt(Math.random()*randomRange)+ parseInt(constraints.minVal))
+      }
+      
+    }
+    
+    setRes( tempArr.map(val => <span> {val}</span>))
+    
+  }
+
+
   return (
     <div className='caseBody'>
-      
       <div className='inputSection'>
         <div className='eachInput'>
-          <label for='minSize'>Enter minimum number of integers to generate</label>
-          <input type='text' placeholder='Min number of values' name='minSize' />
+          <label htmlFor='size'>Enter number of integers to generate</label>
+          <input type='number' placeholder='Min number of values' name='size' onChange={handleInput}/>
         </div>
         <div className='eachInput'>
-          <label for='maxSize'>Enter maximum number of integers to generate</label>
-          <input type='text' placeholder='Max number of values' name='maxSize' />
+          <label htmlFor='minVal'>Enter minimum value of an integer</label>
+          <input type='number' placeholder='Min integer value' name='minVal' onChange={handleInput} />
         </div>
         <div className='eachInput'>
-          <label for='minVal'>Enter minimum value of an integer</label>
-          <input type='text' placeholder='Min integer value' name='minVal' />
+          <label htmlFor='maxVal'>Enter maximum value an integer</label>
+          <input type='number' placeholder='Max integer value' name='maxVal' onChange={handleInput} />
         </div>
         <div className='eachInput'>
-          <label for='maxVal'>Enter maximum value an integer</label>
-          <input type='text' placeholder='Max integer value' name='maxVal' />
+          <label hmtlFor='comma'>Comma separate values?</label>
+          <input type='checkbox' onChange={handleInput}/>
         </div>
-        <div className='eachInput'>
-          <label for='comma'>Comma separate values?</label>
-          <input type='checkbox' />
-        </div>
-        <button>Generate</button>
+        <button onClick={generate}>Generate</button>
       </div>
-      
-
+    
       <div className='outputSection'>
         <div className='resBody'>
-
+          {res}
         </div>
       </div>
-      
     </div>
   )
 }
